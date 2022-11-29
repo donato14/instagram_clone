e51c22e9cef838ad0e61d36d3e3fe6eaade045be
Kang Kujun <donato14@naver.com>
2022-11-29 22:28:44 +0900

    GridView / CustomScrollView

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -10,28 +10,33 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(context.watch<Store2>().name2)),
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: (){
-
-            context.read<Store1>().changeName();
-          }, child: Text('버튼')),
-          Container(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.account_circle),
-                Text('팔로워 ${context.watch<Store1>().follower}명'),
-                ElevatedButton(onPressed: (){
-                  context.read<Store1>().changeFollower();
-                }, child: Text('팔로우')),
-                ElevatedButton(onPressed: (){
-                  context.read<Store1>().getData();
-                }, child: Text('사진가져오기'))
-              ],
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(Icons.account_circle),
+                  Text('팔로워 ${context.watch<Store1>().follower}명'),
+                  ElevatedButton(onPressed: (){
+                    context.read<Store1>().changeFollower();
+                  }, child: Text('팔로우')),
+                  ElevatedButton(onPressed: (){
+                    context.read<Store1>().getData();
+                  }, child: Text('사진가져오기'))
+                ],
+              ),
             ),
-          )
+          ),
+          SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                  (c,i) => Image.network(context.watch<Store1>().profileImage[i]),
+                childCount: context.watch<Store1>().profileImage.length,
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          ),
         ],
       ),
     );
